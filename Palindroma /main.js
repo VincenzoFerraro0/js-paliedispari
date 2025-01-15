43cb8324f714b531b78db163208692919053cc11
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-15 13:00:38 +0100

    add: Task, Folder Palindroma

---
+++ Palindroma /main.js
@@ -0,0 +1,27 @@
+/* 
+Palindroma:
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+Consigli del giorno:
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+Domande da farsi quando si crea una funzione:
+Come dovrebbe chiamarsi?
+Ho bisogno di parametri?
+Devo restituire un valore?
+Se sì, di che tipo?
+Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
+
+*/
+
+/*
+    MINI TASK:
+    - creiamo una variabile con la funzione prompt e chiediamo di inserire una parola 
+    - creaiamo una funzione 
+    - all' interno della funzione dichiariamo una variabile che corrisponderà alla parolaUtente invertita
+    - Confrontiamo la parolaUtente originale con la versione invertita.
+    - Chiamiamo la funzione con la parola inserita dall’utente.
+    - Stampiamo il risultato
+
+ */
+
